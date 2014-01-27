336b9e0e3ea41d4082640547cec6f1b87014d728
dimfishr <dimfish@gmail.com>
2014-01-27 14:50:19 +0400

    Disable Touch by Proximity Sensor
    Support Sweep2Wake/DoubleTap2wake/Knock2Wake features in custom
    kernels which prevent touch from sleeping when screen is off.
    
    Change-Id: Ic5bdaf63ffe7502ad164b6e3022ffe8393011e1e
    
    Conflicts:
    	src/com/android/incallui/InCallActivity.java

--- src/com/android/incallui/InCallActivity.java
+++ src/com/android/incallui/InCallActivity.java
@@ -29,6 +29,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -322,6 +323,13 @@ public class InCallActivity extends Activity {
 
     public CallButtonFragment getCallButtonFragment() {
         return mCallButtonFragment;
+	}
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) { // On touch.
+        if (InCallPresenter.getInstance().getProximitySensor().isScreenOffByProximity())
+            return true; 
+
+        return super.dispatchTouchEvent(event);
     }
 
     private void internalResolveIntent(Intent intent) {
